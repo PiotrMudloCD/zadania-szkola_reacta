4a0c1c77c8e2e2398cf89f041dead6304517b783
Piotr <piotr.mudlo@vmlyr.com>
2022-05-22 13:01:15 +0200

    add Lucky-strike

---
+++ lucky-strike/index.js
@@ -0,0 +1,12 @@
+const prompt = require('prompt');
+const enteredNumber = Math.floor((Math.random() * 10) + 1);
+
+prompt.start();
+
+prompt.get(['number'], function (err, result) {
+  if (result.number ==parseInt(enteredNumber) && enteredNumber !== '') {
+    console.log(`Brawo!!! ${enteredNumber} = ${result.number}`);
+  } else {
+    console.log(`${enteredNumber} nie równa się ${result.number}, spróbuj jeszcze raz :)`);
+  };
+});
